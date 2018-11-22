df49c2d4e2a9287a4ae0f6cee0be40dd03d273f3
Zhang Handuo <hzhang032@e.ntu.edu.sg>
2018-11-22 16:37:11 +0800

    :sparkles: add orientation alignment for GPS

--- src/pose_to_path.cpp
+++ src/pose_to_path.cpp
@@ -1,6 +1,11 @@
 #include <ros/ros.h>
 #include <nav_msgs/Path.h>
 #include <geometry_msgs/PoseWithCovarianceStamped.h>
+#include <geometry_msgs/Quaternion.h>
+#include <geometry_msgs/Transform.h>
+#include <eigen_conversions/eigen_msg.h>
+#include <eigen3/Eigen/Core>
+#include <eigen3/Eigen/Geometry>
 
 class PathBuilder
 {
@@ -9,12 +14,12 @@ class PathBuilder
     PathBuilder();
 
   private:
-
+    Eigen::Quaterniond rot_g0_inverse_;
+    Eigen::Vector3d offset_;
     unsigned int frame_no_;
     bool external_frame_, init_origin_;
     std::string frame_id_;
     ros::NodeHandle nh_, nhp_;
-    double x, y, z;
 
     ros::Subscriber pose_sub_;
     ros::Publisher path_pub_;
@@ -27,7 +32,7 @@ class PathBuilder
 };
 
 PathBuilder::PathBuilder()
-  : frame_no_(0), external_frame_(false), nhp_("~"), x(0), y(0), z(0)
+  : frame_no_(0), external_frame_(false), nhp_("~")
 {
   if(nhp_.getParam("frame_id", frame_id_)) {
     external_frame_ = true;
@@ -53,22 +58,45 @@ void PathBuilder::poseCallback(const geometry_msgs::PoseWithCovarianceStamped::C
   aux.pose = pose_msg->pose.pose;
   if(init_origin_){
 
-      if(frame_no_ == 0){
+      Eigen::Quaterniond rot_gk_(pose_msg->pose.pose.orientation.w, pose_msg->pose.pose.orientation.x,
+                                 pose_msg->pose.pose.orientation.y, pose_msg->pose.pose.orientation.z);
+      Eigen::Vector3d trans_gk_(pose_msg->pose.pose.position.x, pose_msg->pose.pose.position.y, pose_msg->pose.pose.position.z);
 
-          x = pose_msg->pose.pose.position.x;
-          y = pose_msg->pose.pose.position.y;
-          z = pose_msg->pose.pose.position.z;
+      if(frame_no_ == 0)
+          rot_g0_inverse_ = rot_gk_.inverse();
+
+      Eigen::Isometry3d gk2enu = Eigen::Isometry3d::Identity();
+      Eigen::Isometry3d fake_cam_frame_pose = Eigen::Isometry3d::Identity();
+      gk2enu.linear() = rot_gk_.toRotationMatrix();
+      gk2enu.translation() = trans_gk_;
+
+      fake_cam_frame_pose = rot_g0_inverse_ * gk2enu;
+      fake_cam_frame_pose.translation().z() = - fake_cam_frame_pose.translation().z();
+      Eigen::Quaterniond new_rot(fake_cam_frame_pose.matrix().topLeftCorner<3,3>());
+
+      if(frame_no_ == 0){
+          offset_ = fake_cam_frame_pose.translation();
+
           aux.pose.position.x = 0;
           aux.pose.position.y = 0;
           aux.pose.position.z = 0;
 
           frame_no_ ++;
       }else{
-          aux.pose.position.x = pose_msg->pose.pose.position.x - x;
-          aux.pose.position.y = pose_msg->pose.pose.position.y - y;
-          aux.pose.position.z = pose_msg->pose.pose.position.z - z;
+          Eigen::Vector3d new_trans(fake_cam_frame_pose.translation() - offset_);
+          aux.pose.position.x = new_trans.x();
+          aux.pose.position.y = new_trans.y();
+          aux.pose.position.z = new_trans.z();
       };
+
+      aux.pose.orientation.w = new_rot.w();
+      aux.pose.orientation.x = new_rot.x();
+      aux.pose.orientation.y = new_rot.y();
+      aux.pose.orientation.z = new_rot.z();
   }
+//  else {
+//      aux.pose = pose_msg->pose.pose;
+//  }
 
   path_msg_.poses.push_back( aux );
 
